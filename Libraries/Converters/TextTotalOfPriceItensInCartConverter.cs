0a684ccf3144c0f022112e5a4686a98ee0b181e0
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-02-15 11:52:27 -0300

    conversor de preco

---
+++ Libraries/Converters/TextTotalOfPriceItensInCartConverter.cs
@@ -0,0 +1,34 @@
+﻿using AppListaDeCompras.Models;
+using System.Globalization;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+	public class TextTotalOfPriceItensInCartConverter : IValueConverter
+	{
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			ListToBuy listToBuy = value as ListToBuy;
+
+			if (listToBuy is null)
+				return "R$ 0,00";
+
+			if (listToBuy.Products.Count == 0)
+				return "R$ 0,00";
+
+			decimal totalPrice = 0;
+
+			foreach (var product in listToBuy.Products)
+			{
+				if (product.HasCaught)
+					totalPrice += product.Price * product.Quantity;
+			}
+
+			return totalPrice.ToString("C");
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
