0412b02836ac4206721241f03e835cdf200f2df1
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-02-29 16:10:02 -0300

    validacoes da inclusao/edicao de itens e do nome da lista

---
+++ Libraries/Utilities/Validator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System.Text;
+
+namespace AppListaDeCompras.Libraries.Utilities
+{
+	public class Validator
+	{
+		public static string ShowErrorMessage(ValidationResult result)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (var failure in result.Errors)
+			{
+				sb.AppendLine($"{failure.ErrorMessage}");
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ Libraries/Validations/AddItemValidator.cs
@@ -0,0 +1,28 @@
+﻿using AppListaDeCompras.Models;
+using FluentValidation;
+
+namespace AppListaDeCompras.Libraries.Validations
+{
+	public class AddItemValidator : AbstractValidator<Product>
+	{
+		public AddItemValidator() 
+		{
+			RuleFor(x => x.Name)
+				.NotEmpty().WithMessage("O campo 'nome' é obrigatório!")
+				.MinimumLength(3).WithMessage("O campo 'nome' deve ter pelo menos 3 caracteres!");
+
+			RuleFor(x => x.Quantity)
+				.NotEmpty().WithMessage("O campo 'quantidade' é obrigatório!")
+				.Must(MoreThanOne).WithMessage("O campo 'quantidade' deve ser maior que 1!");
+
+			RuleFor(x => x.Price)
+				.NotEmpty().WithMessage("O campo 'preço' é obrigatório!")
+				.Must(MoreThanOne).WithMessage("O campo 'preço' deve ser maior que 1!");
+		}
+
+		private bool MoreThanOne(decimal quantity)
+		{
+			return quantity > 0;
+		}
+	}
+}
