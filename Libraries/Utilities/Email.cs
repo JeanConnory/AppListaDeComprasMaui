130ab1c93f5db3b1caed493510a48899114de99b
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-02-29 22:36:49 -0300

    Envio de email com access code

---
+++ Libraries/Utilities/Email.cs
@@ -0,0 +1,21 @@
+﻿using AppListaDeCompras.Models;
+using System.Net.Mail;
+
+namespace AppListaDeCompras.Libraries.Utilities
+{
+	public class Email
+	{
+		public static void SendEmailWithAccessCode(User user)
+		{
+			var smtp = App.Current.MainPage.Handler.MauiContext.Services.GetRequiredService<SmtpClient>();
+
+			MailMessage msg = new MailMessage();
+			msg.From = new MailAddress("michaelrhcp@gmail.com");
+			msg.To.Add(user.Email);
+			msg.Subject = "App Lista de Compras - Código de Acesso";
+			msg.Body = $"Seu código de acesso é {user.AccessCodeTemp}";
+
+			smtp.Send(msg);
+		}
+	}
+}
